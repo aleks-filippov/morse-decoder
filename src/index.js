3b6dc14d6a993cdc28c768e34d27b584ed1c2ecb
aleks-filippov <108218140+aleks-filippov@users.noreply.github.com>
2023-08-22 14:33:10 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let str = '';
+  let slc = '';
+
+  while (expr.length > 0) {
+    if (expr.length === 10) {
+      slc = expr;
+      expr = '';
+    } else {
+      slc = expr.slice(0, 10);
+      expr = expr.slice(10);
+    }
+
+    if (slc === '**********') str += ' ';
+    else {
+      let arr = [];
+      for (let i = 0; i <= 8; i += 2) {
+        let temp = '';
+        temp += slc[i] + slc[i + 1];
+        arr.push(temp);
+      }
+
+      let ex = '';
+      for (let i = 0; i < 5; i++) {
+        if (arr[i] === '00') continue;
+        else if (arr[i] === '10') ex += '.';
+        else ex += '-';
+      }
+
+      str += MORSE_TABLE[ex];
+    }
+  }
+  return str;
 }
 
 module.exports = {
